520a7935102d771a2d283045ec526f2eee523cc0
lagessiehcs <lagessiehcs.git@gmail.com>
2025-10-23 01:17:07 +0200

    exit launch on process exit

--- src/py_control_panel_package/launch/control_panel_launch.py
+++ src/py_control_panel_package/launch/control_panel_launch.py
@@ -1,6 +1,9 @@
 import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import RegisterEventHandler, EmitEvent
+from launch.event_handlers import OnProcessExit
+from launch.events import Shutdown
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
@@ -8,19 +11,32 @@ def generate_launch_description():
         get_package_share_directory('py_control_panel_package'),
         'config',
         'launch_params.yaml'
+    )
+
+    control_panel_node = Node(
+        package='py_control_panel_package',
+        executable='control_panel',
+        name='py_control_panel',
+        parameters=[config],
+        output='screen'
+    )
+
+    interface_node = Node(
+        package='cpp_interface_package',
+        executable='interface_pubsub',
+        name='cpp_interface'
+    )
+
+    # Shutdown the launch when control_panel exits
+    shutdown_on_exit = RegisterEventHandler(
+        OnProcessExit(
+            target_action=control_panel_node,
+            on_exit=[EmitEvent(event=Shutdown())]
         )
-    
+    )
+
     return LaunchDescription([
-        Node(
-            package='cpp_interface_package',
-            executable='interface_pubsub',
-            name='cpp_interface'
-        ),
-        Node(
-            package='py_control_panel_package',
-            executable='control_panel',
-            name='py_control_panel',
-            parameters=[config],
-            output='screen'
-        ),
+        control_panel_node,
+        interface_node,
+        shutdown_on_exit,
     ])
